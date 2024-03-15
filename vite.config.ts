75a9644aeb57b38527bc7ab68029e656115b35d2
claytonbrgs <142549801+claytonbrgs@users.noreply.github.com>
2024-03-15 18:29:45 -0300

    hostinger fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // Set this to './' for relative paths
   plugins: [react()],
   optimizeDeps: {
     exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
