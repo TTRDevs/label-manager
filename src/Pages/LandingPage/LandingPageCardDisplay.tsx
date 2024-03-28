8ea54ebe320a069e8fb777f30647bf68ca50e9c9
Raphael Palmer <palmer@Raphaels-Mac-Studio.local>
2024-03-28 20:35:48 -0300

    layout fix

--- src/Pages/LandingPage/LandingPageCardDisplay.tsx
+++ src/Pages/LandingPage/LandingPageCardDisplay.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import LandingPageIndividualCard from "./LandingPageIndividualCard";
 import Box from '@mui/material/Box';
 import moduleCardData from './modulesCardData';
@@ -6,23 +7,22 @@ import useMediaQuery from '@mui/material/useMediaQuery';
 
 export default function LandingPageCardDisplay() {
     const theme = useTheme();
-    const matches = useMediaQuery(theme.breakpoints.down('md'));
+    const isSmallScreen = useMediaQuery('(max-width:900px)');
+    const isVerySmallScreen = useMediaQuery('(max-width:500px)'); // Adjust the pixel value as needed
 
     const moduleNames = Object.keys(moduleCardData); 
 
     return (
         <Box sx={{ 
-            display: 'grid',
-            position: 'relative', // Add a relative position to ensure the grid doesn't overflow the container
-            gridTemplateColumns: matches ? 'repeat(1, 1fr)' : 'repeat(4, 1fr)',
+            display: 'grid', 
+            gridTemplateColumns: isVerySmallScreen ? '1fr' : isSmallScreen ? 'repeat(2, 1fr)' : 'repeat(4, 1fr)',
             gap: 2,
             justifyContent: 'center',
             alignItems: 'start',
             maxWidth: '1200px',
-            minWidth: '400px',
-            margin: '1rem auto', // Add a top margin to ensure cards don't go under the header
+            margin: '20px auto',
             gridAutoFlow: 'row dense',
-            padding: '2rem', // Add padding to maintain space around the edges
+            padding: theme.spacing(2), // Adjust padding if necessary
         }}>
             {moduleNames.map((name) => (
                 <LandingPageIndividualCard key={name} title={name} />
